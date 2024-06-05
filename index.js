7198f6e7cd524ab894eba19e94949768a46a8243
robiul-romjan <robiulromjan7@gmail.com>
2024-06-06 01:09:52 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,156 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+
+// middleware
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vhjeumo.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // collection name
+    const questionCollection = client.db("quizApp").collection("questions");
+    const usersCollection = client.db("quizApp").collection("users");
+    const resultsCollection = client.db("quizApp").collection("quizResults");
+
+    // get all quizzes
+    app.get("/all-questions", async (req, res) => {
+      const result = await questionCollection.find().toArray();
+      res.send(result);
+    });
+
+    // post question
+    app.post("/add-question", async (req, res) => {
+      const newQuestion = req.body;
+      const result = await questionCollection.insertOne(newQuestion);
+      res.send(result);
+    });
+
+    //get all users
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    // save user Email and name in DB
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "User already exists" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    //   make teacher
+    app.patch("/users/teacher/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "Teacher",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+     //   make user admin
+     app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "Admin"
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+       // get my quizzes
+       app.get("/my-quizzes", async (req, res) => {
+        let query = {};
+        if (req.query?.email) {
+          query = { createdBy: req.query.email }
+        }
+        const result = await questionCollection.find(query).toArray();
+        res.send(result);
+    });
+
+     // get quiz results
+    app.get("/my-results", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email }
+      }
+      const result = await resultsCollection.find(query).toArray();
+      res.send(result);
+  });
+      
+     // post quiz results
+     app.post("/add-results", async (req, res) => {
+      const quizResults = req.body;
+      const result = await resultsCollection.insertOne(quizResults);
+      res.send(result);
+    });
+
+
+     // delete quiz
+     app.delete("/quiz/:id", async(req, res)=> {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await questionCollection.deleteOne(query);
+      res.send(result)
+    });
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Quiz app Server is running..");
+});
+
+app.listen(port, () => {
+  console.log(`Quiz app Server running on port ${port}`);
+});
